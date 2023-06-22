0275898e315523af72e675553530aa2d94f6ccd3
den1slx <keycoingold@mail.ru>
2023-06-22 22:30:45 +1000

    Add save_user_in_db

--- app/tg_bot/bot_functions.py
+++ app/tg_bot/bot_functions.py
@@ -3,23 +3,34 @@ from textwrap import dedent
 
 from tg_bot.models import Event, Lecture
 from tg_bot.bot_env import telebot, bot, chats
-from tg_bot.bot_markups import speaker_menu_markup, user_menu_markup, registrate_markup, accept_markup, remove_markup, \
-    event_menu_markup
+from tg_bot.bot_markups import speaker_menu_markup, user_menu_markup, registrate_markup, accept_markup, remove_markup,\
+    admin_menu_markup
+from tg_bot.models import Particiant
 
 
 def is_registered_user(chat_id):
     # TODO check user in db
-    return False
+    return True
 
 
 def is_speaker(chat_id):
     # TODO check user status
-    return True
+    return False
+
+
+def is_admin(chat_id):
+    # TODO check user status
+    return False
 
 
 def get_info(message: telebot.types.Message):
     # TODO add info
     bot.send_message(message.chat.id, f'Информация о нас')
+    return
+
+
+def save_user_in_db(tg_id, fullname, mail):
+    Particiant.objects.get_or_create(telegram_id=tg_id, name=fullname, email=mail)
     return
 
 
@@ -39,7 +50,9 @@ def get_schedule(message: telebot.types.Message):
         lectures = Lecture.objects.filter(event=event)
         for lecture in lectures:
             message_text += f"{lecture.start.strftime('%H:%M')} - {lecture.title}\n"
-    bot.edit_message_text(message_text, message.chat.id, message.id, parse_mode='HTML', reply_markup=registrate_markup)
+    # bot.edit_message_text(message_text, message.chat.id, message.id, parse_mode='HTML', reply_markup=remove_markup)
+    #  Убрал маркап тк расписание также используется у зарегистрированных пользователей  # TODO clean up
+    bot.edit_message_text(message_text, message.chat.id, message.id, parse_mode='HTML')
     # bot.send_message(message.chat.id, message_text, parse_mode='HTML', reply_markup=registrate_markup)
     return
 
@@ -65,6 +78,7 @@ def registrate_user(message: telebot.types.Message, step=0):
             msg = bot.send_message(message.chat.id, f'Введите вашу почту', reply_markup=remove_markup)
             bot.register_next_step_handler(msg, registrate_user, 3)
         else:
+            bot.send_message(message.chat.id, f'Используйте команду /start', reply_markup=remove_markup)
             return
     if step == 3:
         msg = bot.send_message(message.chat.id, f'Ваша почта: {message.text}', reply_markup=accept_markup)
@@ -75,6 +89,7 @@ def registrate_user(message: telebot.types.Message, step=0):
             msg = bot.send_message(message.chat.id, f'Введите ваш телефон', reply_markup=remove_markup)
             bot.register_next_step_handler(msg, registrate_user, 5)
         else:
+            bot.send_message(message.chat.id, f'Используйте команду /start', reply_markup=remove_markup)
             return
     if step == 5:
         msg = bot.send_message(message.chat.id, f'Ваш телефон: {message.text}', reply_markup=accept_markup)
@@ -82,12 +97,15 @@ def registrate_user(message: telebot.types.Message, step=0):
         bot.register_next_step_handler(msg, registrate_user, 6)
     if step == 6:
         if message.text == 'Подтвердить':
-            # TODO variables for db: user["id"], user["fullname"], user["phonenumber"], user["mail"]
+            save_user_in_db(user['id'], user['fullname'], user['mail'])
             msg = bot.send_message(
                 message.chat.id,
                 'Регистрация завершена. Используйте команду /start',
                 reply_markup=remove_markup
             )
+            return
+        else:
+            bot.send_message(message.chat.id, f'Используйте команду /start', reply_markup=remove_markup)
             return
         return
 
@@ -113,6 +131,11 @@ def start_bot(message: telebot.types.Message):
                 message.chat.id,
                 f'Здравствуйте, {message.from_user.username}',
                 reply_markup=speaker_menu_markup)
+        elif is_admin(user_id):
+            bot.send_message(
+                message.chat.id,
+                f'Здравствуйте, {message.from_user.username}',
+                reply_markup=admin_menu_markup)
         else:
             bot.send_message(
                 message.chat.id,

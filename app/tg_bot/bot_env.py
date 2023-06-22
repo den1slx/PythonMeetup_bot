fee2bc327ffe8e39614957241fef55fcf531d4b5
mivanov06 <mivanov06@yandex.ru>
2023-06-22 16:32:04 +1000

    added command for menu

--- app/tg_bot/bot_env.py
+++ app/tg_bot/bot_env.py
@@ -6,3 +6,7 @@ env = Env()
 env.read_env()
 tg_bot_token = env('TG_CLIENTS_TOKEN')
 bot = telebot.TeleBot(tg_bot_token)
+
+bot.set_my_commands([
+    telebot.types.BotCommand("/start", "В начало"),
+])

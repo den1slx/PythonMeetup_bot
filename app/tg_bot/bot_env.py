28ab47d35f5111a62d4013d13218d5308ae0f4be
mivanov06 <mivanov06@yandex.ru>
2023-06-23 16:46:24 +1000

    added description for bot

--- app/tg_bot/bot_env.py
+++ app/tg_bot/bot_env.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 from environs import Env
 import telebot
 
@@ -9,5 +11,15 @@ bot = telebot.TeleBot(tg_bot_token)
 bot.set_my_commands([
     telebot.types.BotCommand("/start", "В начало"),
 ])
+
+description_text = '''
+        - зарегистрировать на Python мипап
+        - узнать расписание митапа
+        - задать вопросы докладчикам во время выступлений
+        - оперативно получить информацию об изменениях
+        - быть в курсе о следующих мероприятиях 
+'''
+
+bot.set_my_description(dedent(description_text))
 
 chats = {}
